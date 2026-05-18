1d43d3220a86f617247b54dba8e27903c90d105b
jasim <jasimtp786@gmail.com>
2026-05-18 18:28:19 +0530

    add vercel frontend url

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ require("dotenv").config();
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    "https://shopco-frontend-u1m5.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
