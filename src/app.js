ff2fb01cacd419f9c07f9e4314930f3972a4779a
jasim <jasimtp786@gmail.com>
2026-05-18 16:12:48 +0530

    fix cors properly

--- src/app.js
+++ src/app.js
@@ -4,14 +4,23 @@ require("dotenv").config();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://localhost:5173",
+];
+
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
-);app.use(express.json());
+);
 
+app.options(/.*/, cors());
+
+app.use(express.json());
 
 
 
